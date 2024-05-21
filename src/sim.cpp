4bd8bbb5f99daa569a04670b93f8885051fcfea4
plarotta <plarottaone2@gmail.com>
2024-05-21 15:34:30 -0400

    completed basic simulation module and added time feedback

--- src/sim.cpp
+++ src/sim.cpp
@@ -8,7 +8,7 @@
 #include "sim.hpp"
 #include "robot.hpp"
 #include <vector>
-
+#include <iomanip>
 
 using namespace Eigen;
 using namespace std;
@@ -29,11 +29,11 @@ Simulation::Simulation (float a, int b, float c, float d, float e) {
     }
 }
 Simulation::Simulation() {
-    dt = 0.01;
-    num_robots = 5;
+    dt = 0.05;
+    num_robots = 300;
     mu_k = 0.7;
     mu_s = 0.8;
-    T = 5;
+    T = 1;
 
     // Resize the vector to hold the desired number of robots
     robots.resize(num_robots);
@@ -44,8 +44,28 @@ Simulation::Simulation() {
     }
 }
 
+void Simulation::run_simulation() {
+    float sim_time = 0.0;
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    for (int i = 0; sim_time < T; i++){
+        
+        auto current_time = std::chrono::high_resolution_clock::now();
+        auto elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(current_time - start_time).count();
+
+        cout << "SIM TIME: " << sim_time << " | " <<"REAL TIME: " << std::fixed << std::setprecision(2) << elapsed_time << endl;
+        // update each robot in the simulation
+        for (int r_idx = 0; r_idx < num_robots; r_idx ++){
+            robots[r_idx].compute_spring_forces(sim_time);
+            robots[r_idx].force_integration();
+        }
+        sim_time += dt;
+    }
+}
+
 int main () {
     Simulation simy = Simulation();
-    cout << "SUCCESS!" << endl;
+    cout << "Started sim..." << endl;
+    simy.run_simulation();
     return 0;
 }
